30c53355b1f11a0bc29b8e018940826b8f8cb74e
Darkmift <darkmift@gmail.com>
2023-12-14 20:15:18 +0200

    [main] Add consts.ts file and update types/index.ts

---
+++ hello-world/config/consts.ts
@@ -0,0 +1,42 @@
+import { LanguageMap } from '../types';
+
+export const languageMap: LanguageMap = {
+    hebrew: {
+        volunteerLangColId: 'checkbox4',
+        helpRequesterLangColId: 'checkbox',
+    },
+    russian: {
+        volunteerLangColId: 'checkbox',
+        helpRequesterLangColId: 'check',
+    },
+};
+
+export const MONDAY_API_URL = 'https://api.monday.com/v2';
+
+/**
+ * group raw list to move requesters who were not assigned
+ */
+export const GROUP_RAW_LIST = 'topics';
+
+/**
+ * This is the group id in the volunteer board
+ * where volunteers are grouped for active assignment
+ */
+export const COLUMN_ASSIGN_REQUESTER_TOVOLUNTEER = 'board_relation';
+/**
+this is the column in the requesters board
+ where the assign volunteer id is listed as board_relation
+ */
+export const COLUMN_ASSIGN_VOLUNTEER_TO_REQUESTER = 'connect_boards5';
+/**
+ * the id of the volunteers board
+ */
+export const VOLUNTEER_BOARD_ID = 1316808337;
+/**
+ * column id for capacity
+ */
+export const COLUMN_CAPACITY = 'numbers';
+/**
+ * group id to assign the request helper to
+ */
+export const GROUP_AWAITING_CALL_FROM_VOLUNTEER = 'new_group73364';
--- hello-world/types/index.ts
+++ hello-world/types/index.ts
@@ -1,3 +1,8 @@
+export type LanguageMap = Record<
+    string,
+    { volunteerLangColId: string; helpRequesterLangColId: string; hCanSpeak?: boolean }
+>;
+
 export interface IAssignVolunteerEvent {
     body: Body;
     headers: Headers;
@@ -7,13 +12,13 @@ export interface IAssignVolunteerEvent {
 }
 
 export interface Body {
-    event: Event;
+    event: MondayEvent;
 }
 
-export interface Event {
+export interface MondayEvent {
     app: string;
     type: string;
-    triggerTime: Date;
+    triggerTime: string; // of a date
     subscriptionId: number;
     userId: number;
     originalTriggerUuid: null;
@@ -23,6 +28,7 @@ export interface Event {
     destGroupId: string;
     destGroup: DestGroup;
     triggerUuid: string;
+    challenge?: string;
 }
 
 export interface DestGroup {
@@ -43,4 +49,73 @@ export interface Headers {
     'X-Forwarded-For': string;
     'X-Forwarded-Port': string;
     'X-Forwarded-Proto': string;
+}
+
+// GQL query & mutation params
+
+export type MoveHelpRequesterBackToRawListGroupVariables = {
+    helpRequesterId: number;
+    groupId: string;
+};
+
+export interface GetVolunteersGroupedByLanguageVariables {
+    boardId: number;
+    langColId: string;
+    capacityColId: string;
+    limit?: number;
+}
+
+export interface SetRequesterMultipleValuesVariables {
+    itemId: number;
+    boardId: number;
+    groupId: string;
+    columnValues: string; // JSON string
+}
+
+// Monday GQL Types
+
+export interface IMatchingVolunteers {
+    boards: {
+        items_page: {
+            items: {
+                id: string;
+                name: string;
+            }[];
+        };
+    }[];
+}
+
+export interface IRequestHelper {
+    complexity: IComplexity;
+    items: Item[];
+}
+
+export interface IComplexity {
+    before: number;
+    after: number;
+    reset_in_x_seconds: number;
+}
+
+export interface Item {
+    name: string;
+    column_values: ColumnValue[];
+}
+
+export interface ColumnValue {
+    id: string;
+    value: null | string;
+}
+
+export interface LinkedPulses {
+    changed_at?: Date;
+    linkedPulseIds: LinkedPulseID[];
+}
+
+export interface LinkedPulseID {
+    linkedPulseId: number;
+}
+
+export interface ICheckedValue {
+    checked: boolean;
+    changed_at: Date;
 }
